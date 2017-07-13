998c4432b84cacf052bcec3d4394271d2ede0b63
peke2 <abem@dc4.so-net.ne.jp>
2017-07-14 01:37:42 +0900

    数字、英数字、記号込みで出力できるようにした。 最終的な文字列でのエスケープとシェルを起動しないときの関数の呼び出しが今後の課題。

--- random/randomstring.erl
+++ random/randomstring.erl
@@ -1,5 +1,5 @@
 - module(randomstring).
-- export([create/1]).
+- export([num/1,num/2, alpha/1,alpha/2, all/1,all/2]).
 
 %	var chars = "abcdefghijklmnopqrstuvwxyz" ++ "ABCDEFGHIJKLMNOPQRSTUVWXYZ" ++ "0123456789" ++ "!\"#$%&'()*+,./:;<=>?[\\]^_`{|}~~-@_"
 %	"abcdefghijklmnopqrstuvwxyz"
@@ -14,8 +14,17 @@ exec(Chars, Num, Count)->
 	I = rand:uniform(Num),
 	[lists:nth(I, Chars)] ++ exec(Chars, Num, Count-1).
 
-create(N)->
-	Chars = "abcdefghijklmnopqrstuvwxyz" ++ "ABCDEFGHIJKLMNOPQRSTUVWXYZ" ++ "0123456789" ++ "!\"#$%&'()*+,./:;<=>?[\\]^_`{|}~~-@_",
+create(N, Chars)->
 	Num = length(Chars),
 	exec(Chars, Num, N).
+
+num(N, C)->	create(N, "0123456789"++C).
+num(N)->num(N, []).
+%num(N)->num(N, []).
+
+alpha(N, C)->num(N, "abcdefghijklmnopqrstuvwxyz" ++ "ABCDEFGHIJKLMNOPQRSTUVWXYZ"++C).
+alpha(N)->alpha(N, []).
+
+all(N, C)->alpha(N, "!\"#$%&'()*+,./:;<=>?[\\]^_`{|}~~-@_"++C).
+all(N)->all(N, []).
 
