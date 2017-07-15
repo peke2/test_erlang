8c1ffa69707fdc938e0a80ddfa0dd6142db59e62
peke2 <abem@dc4.so-net.ne.jp>
2017-07-16 01:37:52 +0900

    文字列の比較ができるかどうかやってみた。普通にできた。

--- match/condition.erl
+++ match/condition.erl
@@ -1,12 +1,26 @@
 - module(condition).
-- export([hello/1]).
+- export([hello/1, check/1]).
 
+
+%erlからの直接実行時
+%	erl -noshell -run condition hello 2 -s init stop
+%erlのシェルから実行時
+%	condition:hello(["2"]).
+%
 hello(N) ->
-	I = erlang:list_to_integer("123"),
-	io:format("Greeting ~w", I),
+	I = list_to_integer(hd(N)),
+	io:format("Greeting ~n"),
 	if I == 1 -> io:format("Hello");
 		I == 2 -> io:format("Good");
 		I == 4 -> io:format("Morning");
 		true -> ""
 	end.
+
+
+%文字列の比較はどうなるのか？
+check(N)->
+	Word = if N=="ABC" -> "Alpha";
+		true -> "Nothing"
+	end,
+	io:format("~s~n", [Word]).
 
