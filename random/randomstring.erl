875c066d66b3ac51d190dee77f2d98309472216c
peke2 <abem@dc4.so-net.ne.jp>
2017-11-14 21:50:44 +0900

    使い方をソースのコメントに追加。

--- random/randomstring.erl
+++ random/randomstring.erl
@@ -1,6 +1,19 @@
 - module(randomstring).
 - export([num/1,num/2, alpha/1,alpha/2, all/1,all/2]).
 
+%	【実行】
+%	erl -noshell -run randomstring <関数名> <文字数>
+%
+%	数字 5文字
+%	erl -noshell -run randomstring num 5
+%
+%	アルファベット＋数字 7文字
+%	erl -noshell -run randomstring alpha 7
+%
+%	記号＋アルファベット＋数字 11文字
+%	erl -noshell -run randomstring all 11
+
+
 %	var chars = "abcdefghijklmnopqrstuvwxyz" ++ "ABCDEFGHIJKLMNOPQRSTUVWXYZ" ++ "0123456789" ++ "!\"#$%&'()*+,./:;<=>?[\\]^_`{|}~~-@_"
 %	"abcdefghijklmnopqrstuvwxyz"
 %	"ABCDEFGHIJKLMNOPQRSTUVWXYZ"
